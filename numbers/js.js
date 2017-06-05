1f0b67de39c054e6cfec153797d9cd5aec014352
vetaluga <vetalugan@mail.ru>
2017-06-05 19:48:04 +0300

    numbers

---
+++ numbers/js.js
@@ -0,0 +1,27 @@
+//
+// for(var num = prompt('Hello'); num < 11; num++){
+//   console.log(num);
+// }
+
+
+var y = 1;
+var x = +prompt('Number');
+
+    while(y <= x){
+    console.log(y);
+    y++;
+  }
+
+// var i = 1;
+// var q = 10;
+//
+// while(i <= q){
+//   console.log( i );
+//   i++;
+// }
+
+
+// do {
+//   console.log(y);
+//   y++
+// } while(x >= y);
