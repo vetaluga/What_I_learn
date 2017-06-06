baddf02b8761af1dd0e63a2b607c71ea3524e875
vetaluga <vetalugan@mail.ru>
2017-06-06 12:56:24 +0300

    addtask

---
+++ numbers devide on 2 and 5/js.js
@@ -0,0 +1,23 @@
+var b = prompt('Enter a number')
+for( var a = 0; b > a; a++){
+  if( a % 3 == 0 || a % 5 == 0){;
+  console.log(a);
+}
+}
+
+
+
+
+
+
+
+
+
+
+// var a = prompt('getElement');
+// var b = 0;
+//
+// while( a >= b ){
+//   console.log( b );
+//   b ++;
+// }
