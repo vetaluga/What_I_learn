a90f19764636965ae55421adcf5b735e7bdbed22
vetaluga <vetalugan@mail.ru>
2017-06-09 20:58:44 +0300

    AddArProg

---
+++ Arithmetic progressionOne/js.js
@@ -0,0 +1,21 @@
+var enNum = prompt('Enter the number');
+
+var a = 1;
+
+var b = 0;
+
+var res;
+
+while( a != enNum ){
+    b = b + a;
+    a++;
+    res = b + a;
+    console.log( res );
+}
+
+
+
+/*нове завдання.
+вхід: 5
+вихід: 15
+рахується так: 1+2+3+4+5*/
